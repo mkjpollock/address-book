717f4e85c12d541c7740bb106c085bcece81d0f4
Michael Grossman & Maigen Skjelstad <student@epicodus.com>
2014-02-24 12:34:36 -0800

    add triangle files from practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,53 @@
-var Contact = { 
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
+var Triangle = {
+  kind: function() {
+    var first = this.firstSide;
+    var second = this.secondSide;
+    var third = this.thirdSide;
+    if (first === second && second === third) {
+      return "Equilateral";
+    } else if ((first === second && second !== third) || (second === third && third !== first) || (first === third && third !== second))  {
+      return "Isosceles";
+    } else if (((first !== second) && (first !== third) && (second !== third)) && ((first + second > third) || (first + third > second) || (second + third > first))) {
+      return "Scalene";
+    } 
+  },
+  invalid: function() {
+    var first = this.firstSide;
+    var second = this.secondSide;
+    var third = this.thirdSide;
+    if ((first > second + third) || (second > first + third) || (third > first + second)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 };
 
-$(document).ready(function(){
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
 
-    var inputFirstName = $("input#new-first-name").val();
-    var inputLastName = $("input#new-last-name").val();
-    var inputAddress = $("input#new-address").val();
-    var newContact = Object.create(Contact);
-    newContact.firstName = inputFirstName;
-    newContact.lastName = inputLastName;
-    newContact.address = inputAddress;
+$(document).ready(function() {
+  $("#new-triangle").submit(function(event) {
+  event.preventDefault();
+    var first = parseInt($("#first-side").val());
+    var second = parseInt($("#second-side").val());
+    var third = parseInt($("#third-side").val());
+    
+    var newTriangle = Object.create(Triangle);
+    newTriangle.firstSide = first;
+    newTriangle.secondSide = second;
+    newTriangle.thirdSide = third;
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    if (newTriangle.invalid() === true) {
+        alert("Those are invalid triangle parameters. Try again.")
+    } else if (newTriangle.kind() === "Equilateral") {
+    $("ul#eqTriangle").append("<li><span class='equal-param'>" + newTriangle.firstSide + ", " + newTriangle.secondSide + ", " + newTriangle.thirdSide + "</span></li>");
+    } else if (newTriangle.kind() === "Isosceles") {
+    $("ul#isTriangle").append("<li><span class='isos-param'>" + newTriangle.firstSide + ", " + newTriangle.secondSide + ", " + newTriangle.thirdSide + "</span></li>");
+    } else if (newTriangle.kind() === "Scalene") {
+    $("ul#scTriangle").append("<li><span class='scal-param'>" + newTriangle.firstSide + ", " + newTriangle.secondSide + ", " + newTriangle.thirdSide + "</span></li>");
+    } 
+
     this.reset();
 
-    $(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.fullName());
-    $(".first").text(newContact.firstName);
-    $(".last").text(newContact.lastName);
-    $(".address").text(newContact.address);
-    });
   });
-
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,34 @@
-describe('Contact', function() {
-  describe('fullName', function() {
-    it('combines the first and last names, with a space in between', function() {
-      var sampleContact = Object.create(Contact);
-      sampleContact.firstName = 'Tom';
-      sampleContact.lastName = 'Waits';
-      sampleContact.fullName().should.equal('Tom Waits');
+describe('Triangle', function() {
+  describe('kind', function() { 
+    it('will tell you if a triangle is Equilateral', function() {
+      var sampleTriangle = Object.create(Triangle);
+      sampleTriangle.firstSide = '4';
+      sampleTriangle.secondSide = '4';
+      sampleTriangle.thirdSide = '4';
+      sampleTriangle.kind().should.equal('Equilateral');
+    });
+    it('will tell you if a triangle is Isosceles', function() {
+      var sampleTriangle = Object.create(Triangle);
+      sampleTriangle.firstSide = '4';
+      sampleTriangle.secondSide = '4';
+      sampleTriangle.thirdSide = '3';
+      sampleTriangle.kind().should.equal('Isosceles');
+    });
+    it('will tell you if a triangle is Scalene', function() {
+      var sampleTriangle = Object.create(Triangle);
+      sampleTriangle.firstSide = '5';
+      sampleTriangle.secondSide = '4';
+      sampleTriangle.thirdSide = '3';
+      sampleTriangle.kind().should.equal('Scalene');
+    });
+  });
+  describe('invalid', function() {
+      it('will tell you if a triangle is not a triangle', function() {
+      var sampleTriangle = Object.create(Triangle);
+      sampleTriangle.firstSide = '10';
+      sampleTriangle.secondSide = '4';
+      sampleTriangle.thirdSide = '4';
+      sampleTriangle.invalid().should.equal(true);
     });
   });
 });
